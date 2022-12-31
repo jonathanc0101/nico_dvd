09a2bd802184dc118534de75c78d1c73005b9cd1
JC <jonathancavia0101@gmail.com>
2022-12-31 03:05:26 -0300

    nicosis

--- sketch.js
+++ sketch.js
@@ -18,25 +18,27 @@ let smackSound;
 let fishMoanSound;
 let hentaiSound;
 
+let lastPositions = [];
+let numberOfPositions = 10;
+
 let r, g, b;
 
 let clackCounter = 0;
 
 function preload() {
-  soundFormats('wav',"mp3");
+  soundFormats("wav", "mp3");
 
   dvds = [];
-  for(let i = 0; i<MAXDVD;i++){
-    dvds.push( loadImage("dvd_logo" + i +".png")   );
+  for (let i = 0; i < MAXDVD; i++) {
+    dvds.push(loadImage("dvd_logo" + i + ".png"));
   }
 
-  smackSound = loadSound('smack.wav');
-  fishMoanSound = loadSound('moaning-fish.mp3');
-  hentaiSound = loadSound('hentai-orgasm-short.mp3');
+  smackSound = loadSound("smack.wav");
+  fishMoanSound = loadSound("moaning-fish.mp3");
+  hentaiSound = loadSound("hentai-orgasm-short.mp3");
 
   dvd = dvds[0];
 }
-
 
 function setup() {
   const speed = 20;
@@ -46,13 +48,14 @@ function setup() {
   xspeed = speed;
   yspeed = speed;
   pickColor();
+
+  setInterval(registrarPos,100);
 }
 
 function pickColor() {
   r = random(100, 256);
   g = random(100, 256);
   b = random(100, 256);
-
 }
 
 function draw() {
@@ -60,22 +63,24 @@ function draw() {
   // rect(x, y, 80, 60);
   // Draw the DVD logo
   tint(r, g, b);
-  image(dvd, x, y);
+  
+  for(let i = 0; i<lastPositions.length; i++){
+    image(dvd, lastPositions[i][0], lastPositions[i][1]);
+  }
 
+  
 
   x = x + xspeed;
   y = y + yspeed;
 
-
-  if( (x + dvd.width >= width) || (x <= 0) || (y + dvd.height >= height) || (y <= 0)){
-    handleClack();    
+  if (x + dvd.width >= width || x <= 0 || y + dvd.height >= height || y <= 0) {
+    handleClack();
   }
 
   if (x + dvd.width >= width) {
     xspeed = -xspeed;
     x = width - dvd.width;
     pickColor();
-  
   } else if (x <= 0) {
     xspeed = -xspeed;
     x = 0;
@@ -93,22 +98,27 @@ function draw() {
   }
 }
 
-function handleClack(){
+function handleClack() {
   clackCounter++;
   newDvd();
-  if(clackCounter % 15 ==0){
+  if (clackCounter % 15 == 0) {
     hentaiSound.play();
   }
-  if(clackCounter % 10 == 0){
+  if (clackCounter % 10 == 0) {
     fishMoanSound.play();
-  }else{
+  } else {
     smackSound.play();
   }
-
-
 }
 
-function newDvd(){
-  dvdIndex ++;
+function registrarPos() {
+  lastPositions.push([x, y]);
+  if (lastPositions.length >= numberOfPositions ) {
+    lastPositions.shift();
+  }
+}
+
+function newDvd() {
+  dvdIndex++;
   dvd = dvds[dvdIndex % dvds.length];
 }

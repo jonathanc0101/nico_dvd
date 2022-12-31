6a2006468b41efc6f09c43f4a9f39bd01d116514
JC <jonathancavia0101@gmail.com>
2022-12-31 02:14:14 -0300

    1st commmit

---
+++ sketch.js
@@ -0,0 +1,111 @@
+// Bouncing DVD Logo
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/131-bouncing-dvd-logo.html
+// https://youtu.be/0j86zuqqTlQ
+
+let x;
+let y;
+
+let xspeed;
+let yspeed;
+
+let dvds;
+let dvdIndex = 0;
+let dvd;
+const MAXDVD = 18;
+
+let smackSound;
+let fishMoanSound;
+
+let r, g, b;
+
+let clackCounter = 0;
+
+function preload() {
+  soundFormats('wav',"mp3");
+
+  dvds = [];
+  for(let i = 0; i<MAXDVD;i++){
+    dvds.push( loadImage("dvd_logo" + i +".png")   );
+  }
+
+  smackSound = loadSound('smack.wav');
+  fishMoanSound = loadSound('moaning-fish.mp3');
+
+
+  dvd = dvds[0];
+}
+
+
+function setup() {
+  const speed = 20;
+  createCanvas(windowWidth, windowHeight);
+  x = random(width);
+  y = random(height);
+  xspeed = speed;
+  yspeed = speed;
+  pickColor();
+}
+
+function pickColor() {
+  r = random(100, 256);
+  g = random(100, 256);
+  b = random(100, 256);
+
+}
+
+function draw() {
+  background(0);
+  // rect(x, y, 80, 60);
+  // Draw the DVD logo
+  tint(r, g, b);
+  image(dvd, x, y);
+
+
+  x = x + xspeed;
+  y = y + yspeed;
+
+
+  if( (x + dvd.width >= width) || (x <= 0) || (y + dvd.height >= height) || (y <= 0)){
+    handleClack();    
+  }
+
+  if (x + dvd.width >= width) {
+    xspeed = -xspeed;
+    x = width - dvd.width;
+    pickColor();
+  
+  } else if (x <= 0) {
+    xspeed = -xspeed;
+    x = 0;
+    pickColor();
+  }
+
+  if (y + dvd.height >= height) {
+    yspeed = -yspeed;
+    y = height - dvd.height;
+    pickColor();
+  } else if (y <= 0) {
+    yspeed = -yspeed;
+    y = 0;
+    pickColor();
+  }
+}
+
+function handleClack(){
+  clackCounter++;
+  newDvd();
+
+  if(clackCounter % 10 == 0){
+    fishMoanSound.play();
+  }else{
+    smackSound.play();
+  }
+
+
+}
+
+function newDvd(){
+  dvdIndex ++;
+  dvd = dvds[dvdIndex % dvds.length];
+}
